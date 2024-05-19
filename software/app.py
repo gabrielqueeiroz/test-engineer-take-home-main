5f081d1e783b7a9130a91df95ed03913d7d88a18
gabrielqueeiroz <gabriel.dequeirozsousa@gmail.com>
2024-05-19 14:22:49 -0400

    implement task 1

--- software/app.py
+++ software/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import numpy as np
 from PIL import Image
 import uuid
+import logging
 from components.camera import CameraMock
 from components.ai import AISystemMock
 from components.database import Database
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +17,11 @@ CORS(app)
 camera = CameraMock()
 ai_system = AISystemMock()
 database = Database("test_results.db")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def nextjs(path):
+    return send_from_directory('.next', path)
 
 @app.route('/capture_image', methods=['POST'])
 def capture_image():
@@ -34,10 +42,14 @@ def capture_image():
 def predict_defect():
     data = request.json
     raw_image = data['raw_image']
+    image_uuid = data['image_UUID']
     image = Image.fromarray(np.array(raw_image, dtype=np.uint8))
     
     defect_present = ai_system.predict(image).item()
     prediction_uuid = uuid.uuid4()
+
+    database.log_result(image_uuid, str(prediction_uuid), defect_present)
+    logging.info(f"Prediction made for image UUID: {image_uuid}, Prediction UUID: {prediction_uuid}, Defect Present: {defect_present}")
 
     return jsonify(has_defect=defect_present, prediction_UUID=str(prediction_uuid))
 
